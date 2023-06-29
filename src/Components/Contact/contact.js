5aaddf8d44eba4e27cbb7e35e9f2b147c445a4d5
varunchandra10 <kola.varunchandra29@gmail.com>
2023-06-29 14:33:11 +0530

    contat\ct update

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -16,7 +16,8 @@ const ContactUs = () => {
             process.env.REACT_APP_YOUR_SERVICE_ID,
             process.env.REACT_APP_YOUR_TEMPLATE_ID,
             form.current,
-            process.env.REACT_APP_YOUR_PUBLIC_KEY)
+            process.env.REACT_APP_YOUR_PUBLIC_KEY,
+            )
             .then((result) => {
                 console.log(result.text);
             }, (error) => {
